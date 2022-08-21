d58f38c2725ab136277d59e483b5285dd3da131b
Eugene <eugeneayonga@gmail.com>
2022-08-21 11:16:08 +0300

    commit tree 0d98d127c2d04bbf441fd94dd0db8095ce2ce986

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
 
+    def initialize
+        super
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
 end
